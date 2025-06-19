390b4516f2c0dc741c6a28a38a1f798871b54e50
Naziya Sultana Mithila <121658415+MithilaKhan@users.noreply.github.com>
2025-06-19 15:33:49 +0600

    Update baseApi.ts

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -7,7 +7,7 @@ const token  = getFromLocalStorage("accessToken")
 export const api = createApi({
     reducerPath: "api",
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://119.148.56.246:5001/api/v1" ,
+        baseUrl: "http://31.97.171.35:5001/api/v1" ,
         // baseUrl: "http://192.168.10.195:5000/api" 
         headers: {
             Authorization: `Bearer ${token}`,
@@ -17,4 +17,4 @@ export const api = createApi({
 });
 
 // export const imageUrl = "http://206.189.231.81:5000";
-export const imageUrl = "http://119.148.56.246:5001/";
+export const imageUrl = "http://31.97.171.35:5001/";
